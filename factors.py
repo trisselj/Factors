af900a203e77a426a2fe51dc7e48f0af3e8ec137
trisselj <trisselj@oregonstate.edu>
2024-07-10 15:45:35 -0700

    fixees

--- factors.py
+++ factors.py
@@ -4,15 +4,15 @@
 # Description: Prints out the factors of an integer entered
 
 #Initial integer request
-Integer = int(input("Please enter a positive integer:"))
+Integer = int(input("Please enter a positive integer: "))
 
 #In case someone enters a negative integer and initial printed response
 if Integer <=0:
-    print("Please print a positive integer, thanks.")
+    print("Please print a positive integer, thanks."1)
 else:
-    print(f"The factor(s) of {Integer} are/is:")
+    print(f"The factors of {Integer} are:")
 
 #Starts with 1 and adds 1 dividing by the numeber each time and checking if there is a remainder before printing if remainder = 0
-for i in range(1, Integer+ 1):
+for i in range(1, Integer + 1):
     if Integer % i == 0:
         print (i)
